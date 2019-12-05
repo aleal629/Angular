89ca4d2ae519cba6fab54d9dc1ad02d9d19779d4
Abraham Leal <aleal629@gmail.com>
2019-12-05 14:27:10 -0500

    Adds first stock/summary and displays it in application

--- stocks/src/app/components/summary/summary.component.ts
+++ stocks/src/app/components/summary/summary.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
 @Component({
-  selector: 'app-summary',
-  templateUrl: './summary.component.html',
-  styleUrls: ['./summary.component.css']
+  selector: 'summary',
+  styleUrls: ['./summary.component.css'],
+  templateUrl: './summary.component.html'
 })
-export class SummaryComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class SummaryComponent {
+  @Input() stock: any;
+  isNegative() {
+    return (this.stock && this.stock.change < 0);
   }
 
+  isPositive() {
+    return (this.stock && this.stock.change > 0);
+  }
 }
