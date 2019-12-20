b9ee067b7f0ad926faea0c9c01c8375b09ac0df5
Abraham Leal <aleal629@gmail.com>
2019-12-20 13:26:26 -0500

    Completion of AinA Ch. 5 activities

--- datacenter-start/src/app/navbar/navbar.component.ts
+++ datacenter-start/src/app/navbar/navbar.component.ts
@@ -3,7 +3,8 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  styles: [`.btn { background-color: #999999; }`]
 })
 export class NavbarComponent implements OnInit {
   @Output() onRefresh: EventEmitter<null> = new EventEmitter<null>();
--- datacenter-start/src/app/nodes-row/nodes-row.component.ts
+++ datacenter-start/src/app/nodes-row/nodes-row.component.ts
@@ -1,5 +1,6 @@
-import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
-
+import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NodesDetailComponent } from '../nodes-detail/nodes-detail.component';
 @Component({
   selector: '[app-nodes-row]',
   templateUrl: './nodes-row.component.html',
@@ -8,7 +9,14 @@ import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
 })
 export class NodesRowComponent {
   @Input() node: any;
+
+  constructor(private modalService: NgbModal) {}
   isDanger(prop) {
     return this.node[prop].used / this.node[prop].available > 0.7;
+  }
+
+  open(node) {
+    const modal = this.modalService.open(NodesDetailComponent);
+    modal.componentInstance.node = node;
   }
 }
