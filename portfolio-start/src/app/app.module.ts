7c3dd1cd37b67ae6d74a40635677facc635266f3
Abraham Leal <aleal629@gmail.com>
2019-12-23 14:46:30 -0500

    AinA Services example

--- portfolio-start/src/app/app.module.ts
+++ portfolio-start/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ClarityModule } from 'clarity-angular';
 import { CurrencyPipe } from '@angular/common';
@@ -13,6 +13,8 @@ import { AlertComponent } from './alert/alert.component';
 
 import { LocalStorageService } from './services/local-storage.service';
 import { AccountService } from './services/account.service';
+import {StocksInterceptor} from './services/interceptor.service';
+import {AlertService} from './services/alert.service';
 
 @NgModule({
   declarations: [
@@ -29,9 +31,15 @@ import { AccountService } from './services/account.service';
     ClarityModule,
   ],
   providers: [
+    AlertService,
     LocalStorageService,
     CurrencyPipe,
     AccountService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: StocksInterceptor,
+      multi: true
+    }
   ],
   bootstrap: [AppComponent]
 })
